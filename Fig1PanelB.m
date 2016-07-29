11a2e478eb6d2bfeb4cfd1b47c3a8c025fd1eeca
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2016-07-28 20:25:28 -0500

    Add files via upload
    this script produces the data (as an .mat file) necessary for fig1_FINAL_plotting.m

---
+++ Fig1PanelB.m
@@ -0,0 +1,26 @@
+%figure 1: aim is to represent the evolution of a 2-state symmetric 
+%environment together with the evolution of the change-point count
+clear
+
+%create environment
+eps=.3;         %change rate
+N=10;           %total number of timesteps
+H=zeros(N,1);   %environment vector
+logicChangePoint=zeros(N-1,1);%presence of change point
+for i=1:N-1
+    logicChangePoint(i)=rand<eps;
+    if logicChangePoint(i)  %force H(1)=0
+        H(i+1)=1-H(i);
+    else
+        H(i+1)=H(i);
+    end
+end
+
+%count change points
+a=[0;cumsum(logicChangePoint)];%force a(1)=0
+b=(0:N-1)'-a;
+
+amplitude=max([a;b]);
+abscissa=1:N;
+
+save('fig1B.mat')
