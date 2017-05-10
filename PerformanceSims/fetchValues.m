b171da387d2f71de553f83e81ca715422351e61b
adrian <adrian@math.uh.edu>
2017-05-09 21:28:14 -0500

    change fetchValues.m since matfile objects don't support {} indexing

--- PerformanceSims/fetchValues.m
+++ PerformanceSims/fetchValues.m
@@ -2,8 +2,8 @@ function [SNR,T,h,alpha,beta]=fetchValues(filename,idx1,idx2,idx3,idx4,idx5)
 % fetches values from the performance array axes, at array location
 % this function does not return a performance value
 matObj=matfile(filename);
-
-Values = matObj.meshValues{idx1,idx2,idx3,idx4,idx5};
+AllValues = matObj.meshValues;
+Values = AllValues{idx1,idx2,idx3,idx4,idx5};
 SNR = Values(1);
 T = Values(2);
 h = Values(3);
