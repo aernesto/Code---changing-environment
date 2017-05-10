acd6331bd307342bda3eaf1fe716d1506e0e0c64
adrian <adrian@math.uh.edu>
2017-05-09 21:03:45 -0500

    create function that gets the parameter values in the array

---
+++ PerformanceSims/fetchValues.m
@@ -0,0 +1,12 @@
+function [SNR,T,h,alpha,beta]=fetchValues(filename,idx1,idx2,idx3,idx4,idx5)
+% fetches values from the performance array axes, at array location
+% this function does not return a performance value
+matObj=matfile(filename);
+
+Values = matObj.meshValues{idx1,idx2,idx3,idx4,idx5};
+SNR = Values(1);
+T = Values(2);
+h = Values(3);
+alpha = Values(4);
+beta = Values(5);
+end
