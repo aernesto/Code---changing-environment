4212af16c1946e5b4e798aecf3f987f480ae89c3
adrian <adrian@math.uh.edu>
2017-05-11 09:20:57 -0500

    panel_plot_perf_SNR_T.m

--- PerformanceSims/panel_plot_perf_SNR_T.m
+++ PerformanceSims/panel_plot_perf_SNR_T.m
@@ -2,7 +2,7 @@
 h=.1:.2:.9;
 for i=1:length(h)
     subplot(2,3,i)
-    plot_perf_SNR_T(h(i),1,1)
+    plot_perf_SNR_T(h(i),1,1, 'perfTable.mat')
 end
 subplot(2,3,6)
-plot_perf_SNR_T(.1,2,1)
+plot_perf_SNR_T(.1,2,1, 'perfTable.mat')
