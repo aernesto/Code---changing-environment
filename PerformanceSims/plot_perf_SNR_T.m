7fa9f3d943218a361a91115308fad16bc8fb798a
adrian <adrian@math.uh.edu>
2017-05-11 09:05:00 -0500

    manage to pcolor plot the Table

--- PerformanceSims/plot_perf_SNR_T.m
+++ PerformanceSims/plot_perf_SNR_T.m
@@ -16,7 +16,13 @@ rows = perfTable.h==hval & perfTable.alpha==alphaval &...
 vars = {'SNR','T','perf'};
 perfMatrix=perfTable{rows,vars};
 
-PLOT=pcolor(perfMatrix(:,2),perfMatrix(:,1),perfMatrix(:,3));
+lt=length(1 : 15 : 500);
+ls=length(.3 : .3 : 3);
+T_mat=reshape(perfMatrix(:,2),lt,ls);
+S_mat=reshape(perfMatrix(:,1),lt,ls);
+perf_mat=reshape(perfMatrix(:,3),lt,ls);
+
+PLOT=pcolor(T_mat,S_mat,perf_mat);
 colormap(bone)
 [cmin,cmax]=caxis;
 caxis([.5,cmax]) % set floor color to perf of 50%
@@ -27,12 +33,12 @@ xlabel('Interrogation time')
 ylabel('SNR')
 ax=gca;
 ax.FontSize=15;
-subsample_T=1:3:lt;
-ax.XTick=subsample_T;
-subsample_SNR=1:2:ls;
-ax.YTick=subsample_SNR;
-ax.XTickLabel=strread(num2str(range_T(subsample_T)),'%s');
-ax.YTickLabel=strread(num2str(range_SNR(subsample_SNR)),'%s');
+% subsample_T=1:3:lt;
+% ax.XTick=subsample_T;
+% subsample_SNR=1:2:ls;
+% ax.YTick=subsample_SNR;
+% ax.XTickLabel=strread(num2str(range_T(subsample_T)),'%s');
+% ax.YTickLabel=strread(num2str(range_SNR(subsample_SNR)),'%s');
 end
 
 
