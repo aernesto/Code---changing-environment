ca15db1005096a0b3da42998accc87dcece721df
adrian <adrian.radillo@gmail.com>
2017-10-26 15:44:44 -0500

    remove commented lines

--- sims_learning_rate/scripts/feedback_effect_1.py
+++ sims_learning_rate/scripts/feedback_effect_1.py
@@ -2,14 +2,9 @@ import matplotlib.pyplot as plt
 plt.rcdefaults()
 import numpy as np
 from scipy.stats import rv_discrete, beta
-# from scipy import stats
 import scipy
-# import random
-# import sqlite3
+import sqlite3
 import datetime
-# import subprocess
-# import scipy.stats
-# import psycopg2
 import dataset
 
 
@@ -107,71 +102,13 @@ class Experiment(object):
                             else:
                                 time_last_cp = int(curr_exp_trial.duration)
                             dec = int(curr_obs_trial.decision)
-                            #                 print(type(dec))
                             correct = bool(dec == curr_exp_trial.end_state)
-                            #                 print(type(correct))
-                            #                 print(dec, correct)
-
-                            # WRITE TO DB
-
-                            #         print(raw_perf)
         if raw_perf:
             self.raw_perf()
 
         # print(perf_lastcp)
         if perf_lastcp:
             self.perf_last_cp()
-
-            # curr_exp_trial.save()
-            #            curr_obs_trial.save()
-            #         self.print_db()
-
-    # def raw_perf(self):
-    #     cursor = cur.execute("SELECT COUNT(Correct) FROM Results WHERE Correct == 1")
-    #     for row in cursor:
-    #         print('percent correct = ', (row[0] / self.tot_trial) * 100)
-    #         print(' ')  # just to have a blank line
-
-    # def perf_last_cp(self):
-    #     #         print('entered the perf_last_cp function')
-    #     # cursor1 = cur.execute('''SELECT TimeLastCp, SUM(Correct)*1.0 / COUNT(TimeLastCp), COUNT(TimeLastCp)
-    #                             FROM Results
-    #                             GROUP BY TimeLastCp
-    #                             ORDER BY TimeLastCp''')
-    #     time = np.zeros(0)
-    #     perf = np.copy(time)
-    #     counts = np.copy(time)
-    #     for row in cursor1:
-    #         time = np.append(time, row[0])
-    #         perf = np.append(perf, row[1])
-    #         counts = np.append(counts, row[2])
-    #     # print(row)
-    #     plt.figure()
-    #     plt.ylim([0, counts.max()])
-    #     plt.bar(time, counts, align='center', width=3)
-    #     plt.title('Histogram of trial counts per time bin')
-    #     plt.xlabel('time since last CP (msec)')
-    #     plt.ylabel('trial count')
-    #
-    #     plt.figure()
-    #     plt.plot(time, perf)
-    #     plt.title('perf as fcn of time since last CP')
-    #     plt.xlabel('time since last CP (msec)')
-    #     plt.ylabel('percent correct')
-    #     plt.show()
-    #
-    # # def print_db(self):
-    # #     print('database content')  # temporary
-    # #     cursor = cur.execute('''SELECT * FROM Results''')
-    # #     for row in cursor:
-    # #         print("Trial nb = ", row[0])
-    # #         print("Decision = ", row[1])
-    # #         print("Correct = ", row[2])
-    # #         print("Time since last cp = ", row[3])
-    # #         print("Trial duration", row[4], "\n")
-    #
-    # def parallel_launch(self):
-    #     return 0  # temporary
 
 
 class ExpTrial(object):
@@ -195,12 +132,6 @@ class ExpTrial(object):
         else:
             return self.expt.switch(self.init_state)
 
-    # def save(self):
-    #        print('stimulus is:')
-    #        print(self.stim)
-
-    # the following is the likelihood used to generate stimulus values,
-    #  given the true state H of the environment
     def randlh(self, H):
         # try clause might be redundant (because switch method does it)
         try:
@@ -220,91 +151,6 @@ class ExpTrial(object):
                 cp_times += [t]
             t += 1
         return np.array(cp_times)
-
-    '''
-    generates poisson train of duration milliseconds with rate true_h in Hz, 
-    using the Gillespie algorithm.
-
-    print statements are only there for debugging purposes
-    '''
-    def gen_cp(self, duration, true_h, printEnvt):
-        # TODO: Generate a warning if >1 ch-pt occur in Experiment.exp_dt window
-        # print('launching gen_cp')
-
-        # convert duration into seconds.
-        secdur = duration / 1000.0
-        # print('secdur = '), secdur
-        '''
-        pre-allocate ten times the mean array size 
-        for speed, will be shrinked after computation
-        '''
-        nEntries = int(np.ceil(10 * true_h * secdur))
-        # print('allocated entries = '), nEntries
-
-        t = np.zeros(nEntries)
-        if nEntries == 0:
-            print('change point times are')
-            print(t)
-            return t
-
-        totalTime = 0
-        eventIdx = -1
-
-        while totalTime < secdur:
-            sojournTime = np.random.exponential(1. / true_h)
-            totalTime += sojournTime
-            eventIdx += 1
-            t[eventIdx] = totalTime
-
-        # trim unused nodes, and maybe last event if occurred beyond secdur
-
-        # print t[0:10]
-        lastEvent, idxLastEvent = t.max(0), t.argmax(0)
-        # print 'lastEvent = ', lastEvent, 'idxLastEvent = ', idxLastEvent
-
-        if lastEvent > secdur:
-            idxLastEvent -= 1
-
-        if idxLastEvent == -1:
-            t = np.zeros((0, 1))
-        else:
-            t = t[0:int(idxLastEvent) + 1]
-
-        if printEnvt:
-            if t.size == 0:
-                x = np.array([0, secdur])
-                y = np.array([self.init_state, self.init_state])
-            else:
-                x = np.insert(t, 0, 0)
-                x = np.append(x, secdur)
-                xpoints = x.size
-                y = np.zeros(xpoints)
-                y[0] = self.init_state
-                last_envt = self.init_state
-                for jjj in np.arange(1, xpoints):
-                    new_envt = self.expt.switch(last_envt)
-                    if jjj == xpoints - 1:
-                        y[jjj] = last_envt
-                    else:
-                        y[jjj] = new_envt
-                    last_envt = new_envt
-
-                    #             print(y)
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            ticks = np.array([0, 1])
-            ax.set_yticks(ticks)
-            ax.set_xticks(x)
-            ax.set_ylim([self.expt.states[0] - 1, self.expt.states[1] + 1])
-            labels = ['H - ', 'H +']
-            plt.yticks(self.expt.states, labels)
-            plt.step(x, y, where='post')
-            plt.title('Environment')
-            plt.xlabel('time (seconds)')
-            plt.show()
-        # print('change point times are')
-        #         print(t)
-        return t
 
 
 class Stimulus(object):
@@ -380,22 +226,8 @@ class IdealObs(object):
     def __init__(self, dt, expt, prior_states=np.array([.5, .5]), prior_h=np.array([1, 1])):
         self.expt = expt  # reference to Experiment object
         self.dt = dt
-        # try:
-        #     if (self.expt.setof_trial_dur % dt) == 0:
-        #         self.dt = dt  # in msec
-        #     else:
-        #         raise AttributeError("Error in arguments: the observer's time"
-        #                              "step size "
-        #                              "'dt' "
-        #                              "does not divide "
-        #                              "the trial durations 'setof_trial_dur'")
-        # except AttributeError as err:
-        #     print(err.args)
-
         self.prior_h = prior_h
         self.prior_states = prior_states  # TODO: check that prior_states is a stochastic vector
-
-        #self.obs_noise = self.expt.setof_stim_noise
 
     # the following is the likelihood used by the ideal observer
     # H = assumed state of the environment
@@ -427,9 +259,6 @@ class ObsTrial(IdealObs):
     def gen_obs(self):
         return self.stimulus.stim
 
-    #     def lh(self, H, x):
-    #         IdealObs.lh(H.x)
-
     def infer(self, printLLR):
         #  initialize variables
         Hp = self.expt.states[1]
@@ -445,8 +274,6 @@ class ObsTrial(IdealObs):
 
         # get first observation
         x = self.obs[0]
-        #         print('first obs = ', x)
-        #         print('Hp = ', Hp)
 
         # First time step
         # compute joint posterior after first observation: P_{t=0}(H,a=0) --- recall first obs at t=0
@@ -472,7 +299,6 @@ class ObsTrial(IdealObs):
             # todo: find a way to exit the function
 
         for j in np.arange(self.stimulus.nbins - 1):
-            #             print('j',j)
             # make an observation
             x = self.obs[j + 1]
 
@@ -481,74 +307,37 @@ class ObsTrial(IdealObs):
             xm = self.lh(Hm, x, self.obs_noise)
 
             # update the boundaries (with 0 and j changepoints)
-            #             print('alpha, priorPrec=',alpha,priorPrec)
             ea = 1 - alpha / (j + priorPrec)
             eb = (j + alpha) / (j + priorPrec)
-            #             print('ea,eb', ea, eb)
             joint_plus_new[0] = xp * ea * joint_plus_current[0]
             joint_minus_new[0] = xm * ea * joint_minus_current[0]
             joint_plus_new[j + 1] = xp * eb * joint_minus_current[j]
             joint_minus_new[j + 1] = xm * eb * joint_plus_current[j]
-            #             if j == 2:
-            #                 print('Hpn', joint_plus_new)
-            #                 print('Hmn', joint_minus_new)
+
             # update the interior values
             if j > 0:
                 vk = np.arange(2, j + 2);
                 #                 print('vk',vk)
                 ep = 1 - (vk - 1 + alpha) / (j + priorPrec)  # no change
                 em = (vk - 2 + alpha) / (j + priorPrec)  # change
-                #                 print('ep, em =', ep, em)
-                #                 if j == 2:
-                #                     print(xp)
-                #                     print(xm)
-                #                     print(joint_plus_current)
-                #                     print(joint_minus_current)
-                #                     print(vk)
-                #                     print(joint_plus_current[vk - 1])
-                #                     print(joint_minus_current[vk - 2])
+
                 joint_plus_new[vk - 1] = xp * (np.multiply(ep, joint_plus_current[vk - 1]) +
                                                np.multiply(em, joint_minus_current[vk - 2]))
                 joint_minus_new[vk - 1] = xm * (np.multiply(ep, joint_minus_current[vk - 1]) +
                                                 np.multiply(em, joint_plus_current[vk - 2]))
 
-                # sum probabilities in order to normalize
-            #             if j == 2:
-            #                 print('Hpn', joint_plus_new)
-            #                 print('Hmn', joint_minus_new)
+            # sum probabilities in order to normalize
             Hs = joint_plus_new.sum() + joint_minus_new.sum()
-            #             print('sum Hpn', joint_plus_new.sum())
-            #             print('sum Hmn', joint_minus_new.sum())
-            #             if j == 2:
-            #                 print('Hs = ', Hs)
             joint_plus_current = joint_plus_new / Hs
             joint_minus_current = joint_minus_new / Hs
-            #             if j == 2:
-            #                 print(joint_plus_current)
-            #                 print(joint_minus_current)
             Pp[:, j + 1] = joint_plus_current.copy()
             Pm[:, j + 1] = joint_minus_current.copy()
+
             # compute marginals over state if last iteration
-            #             if j >= self.stimulus.nbins - 3:
             lp = joint_plus_current.sum()
             lm = joint_minus_current.sum()
             self.llr[j + 1] = np.log(lp / lm)
-        # print('j',j)
-        #             if j>0:
-        #                 print('vk',vk)
-        #             print('ea, eb',ea, eb)
-        #             if j>0:
-        #                 print('ep, em',ep, em)
-        #             print('Hpn',joint_plus_new)
-        #             print('Hmn',joint_minus_new)
-        #             print('Hpc',joint_plus_current)
-        #             print('Hmc',joint_minus_current)
 
-
-        #         print(lp)
-        #         print(lm)
-        #         print(Pp)
-        #         print(Pm)
         # compute decision (interrogate the system)
         if np.sign(np.log(lp / lm)) == -1:
             self.decision = Hm
