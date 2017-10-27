11a175e70c949834dda60bbb999ca715bb08cc5a
adrian <adrian.radillo@gmail.com>
2017-10-26 23:23:20 -0500

    stuck on point 4 from visualize_fbdata.py

--- sims_learning_rate/scripts/visualize_fbdata.py
+++ sims_learning_rate/scripts/visualize_fbdata.py
@@ -5,7 +5,7 @@ The questions that this script should answer are:
 1- list existing fields in the DB
 2- list unique values in each field
 3- count how many trials exist per triplet (duration, h, SNR)
-- for fixed triplet, compute the average difference (across trials) between the posterior means
+4- for fixed triplet, compute the average difference (across trials) between the posterior means
     do the same for posterior stdev
 - find the top 10 triplets for which the differences from previous bullet are maximized
 """
@@ -35,11 +35,31 @@ table = db[tablename]
 
 # 3- count how many trials exist per triplet (duration, h, SNR)
 # print('unique values from ' + field)
-result = db.query('SELECT trialDuration, hazardRate, SNR, obsFeedback, COUNT(*) AS c \
+# result = db.query('SELECT trialDuration, hazardRate, SNR, obsFeedback, COUNT(*) AS c \
+# FROM feedback GROUP BY trialDuration, SNR, hazardRate, obsFeedback')
+# for row in result:
+#     print('quadruple:', row['trialDuration'], row['hazardRate'], row['SNR'], row['obsFeedback'])
+#     print('count:', row['c'])
+# print('-----------------------------------')
+
+
+# 4- for fixed triplet, compute the average difference (across trials) between the posterior means
+#     do the same for posterior stdev
+result1 = db.query('SELECT ID, ')
+
+'''
+SELECT
+    a.PurchaseID,
+    ABS(a.PurchaseID - b.PurchaseID) AS diff
+FROM
+    PurchaseID a INNER JOIN PurchaseID b ON a.PurchaseID=b.PurchaseID
+WHERE a.PurchaseID=?
+    AND a.purchaseDate=?
+    AND b.purchaseDate=?'''
+
+
+result = db.query('SELECT trialDuration, hazardRate, SNR, obsFeedback, AVG() AS diff \
 FROM feedback GROUP BY trialDuration, SNR, hazardRate, obsFeedback')
 for row in result:
     print('quadruple:', row['trialDuration'], row['hazardRate'], row['SNR'], row['obsFeedback'])
     print('count:', row['c'])
-print('-----------------------------------')
-
-
