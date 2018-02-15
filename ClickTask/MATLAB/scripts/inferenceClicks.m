f301db76d612948ea3f913eda6e640f2b1d3de65
adrian <adrian.radillo@gmail.com>
2018-02-15 17:14:20 -0600

    sim goal 2 for DT model

--- ClickTask/MATLAB/scripts/inferenceClicks.m
+++ ClickTask/MATLAB/scripts/inferenceClicks.m
@@ -18,108 +18,51 @@ global stimulusLength
     stimulusLength=.010;  % 10 msec
     
 global dt       % time step for time discretization
-    dt=1e-3;   % 1 msec
-    
+    dt=1e-4;   % 0.1 msec
+msect=1000*(dt:dt:stimulusLength);
 global h    % hazard rate, in Hz
     h=4;
 global alpha
     alpha=1;
 global beta
-    beta=1;
     
 global obs  % to be computed later
 global N    % to be computer later
 %create artificial stimulus
-lTrain=[];
-rTrain=.0049;
+lTrain=[];%.006;
+rTrain=[.005];           
 ct=0.004;
-%load('/home/radillo/Git/GitHub/LearningClicksTask/data/ClickTrains_h4_rateHigh26_rateLow14_nTrials1_LONG.mat')
-% number of distinct trial durations in the data array
-%N=size(data,1);
-% get single trial from longest trial duration (3 sec)
-%clicksCell=data{N,1};
-% total number of available trials for this trial duration
-%nTrials = length(clicksCell);
-%trial = 1; % select first trial for now
-%[lTrain,rTrain, ct]=clicksCell{trial, 1:3};
 
-
-% Generate the environment
-%[ct,E]=genClickEnvt();              % this only decides on S+ versus S-
-
-% Generate the observations
-%[lTrain,rTrain]=genClickObs(ct,E);  % this produces the click trains
-
-% if you want to visualize the clicks
-%figure(10) 
-%plotClickTrains(lTrain,rTrain,ct)
-
-
-fig=figure(1); 
-hax=axes; 
-hold on 
+fig=figure(1);  
 SP=rTrain(1)*1000; %right click time in msec
-for snr=[.5,1,2,4]
-    rate_high=getlambdahigh(rate_low, snr, true);
-% perform inference
-    P=jointPosteriorClicks(lTrain,rTrain);
-%compute marginal over state
-    marginalState=squeeze(sum(P,1)); % dim = StateNb x TimeSteps
-
-% plot marginal
-    plot(1000*(dt:dt:stimulusLength), marginalState(1,:),'-o','LineWidth', ...
-        3, 'MarkerSize',4)
+for priorVar=1:3
+    for snr=1:4
+        i=sub2ind([4,3],snr,priorVar);
+        ax=subplot(3,4,i);
+        grid on
+        hold on
+        beta = alpha / sqrt(priorVar);
+        rate_high=getlambdahigh(rate_low, snr, true);
+        % perform inference
+        P=jointPosteriorClicks(lTrain,rTrain);
+        %compute marginal over CP count
+        marginalCPcount=squeeze(sum(P,2)); % dim = CPcount x TimeSteps
+        meanCPcount=(0:size(P,1)-1)*marginalCPcount;
+        stdevCPcount=((0:size(P,1)-1).^2)*marginalCPcount-meanCPcount.^2;
+        % plot marginal
+        plot(msect, meanCPcount,'-b',...
+             msect,meanCPcount+stdevCPcount,'-k',...
+             msect,max(meanCPcount-stdevCPcount,0),'-k',...
+            'LineWidth', 3)
+        line([SP,SP],[0,0.04],'Color',[4,2,3]/4,'LineWidth',2)
+        title(['SNR=',num2str(snr),', Var=',num2str(priorVar)])
+        if snr==1 
+            ylabel('CP count')
+        end
+        if priorVar==3
+            xlabel('msec')
+        end
+        ylim([0,0.04])
+        ax.FontSize=20;
+    end
 end
-title('posterior prob of H+ as fcn of time')
-ylabel('posterior prob(H+)')
-xlabel('time within stimulus (msec)')
-xlim([0,11])
-ylim([0.45,1.05])
-line([SP SP],get(hax,'YLim'),'Color',[1 0 0], 'LineWidth',2)
-line(get(hax,'XLim'),[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
-line(get(hax,'XLim'),[1,1],'Color',[0 0 0], 'LineWidth',1)
-legend('snr=0.5','snr=1','snr=2','snr=4','click time', 'Location', 'east')
-% extract portion of stimulus that contains both 01 and 10 observations
-%wSize = 8; % sliding window size
-%startIdx = 1;
-%endIdx = wSize;
-%found = false;
-% while (endIdx < N) && (~found)
-%     window=obs(startIdx:endIdx,:);
-%     found = sum(ismember([0,1;1,0],window,'rows')) > 1;
-%     if found
-%         break
-%     end
-%     startIdx = endIdx + 1;
-%     endIdx = endIdx + wSize;
-% end
-% endIdx = startIdx + 1;
-% if ~found
-%     fprintf(['no window with 10 and 01 close enough was found\n',...
-%         'default back to window centered at change point\n'])
-%     startIdx = floor(ct(1)/dt);
-%     endIdx = startIdx+wSize;
-% end
-
-
-% plot joint posterior
-
-% figure
-% plotClicksJointPosterior(P,[startIdx,endIdx])
-
-%plot2DClicksJointPosterior(P,[startIdx,endIdx])
-
-% compute posterior mean over change point count
-%mean_cpc = meanPostCPClicks(P); %mean change point count
-
-% plot posterior mean of change point count
-%figure
-%plot((1:length(mean_cpc))*dt,mean_cpc,'LineWidth',3);
-%title('Posterior mean of change point count')
-%xlabel('sec')
-%ylabel('change point count')
-ax = gca;
-ax.FontSize=20;
-% savefile='dt05msec2614.mat';
-% ttime=(1:length(mean_cpc))*dt;
-% save(savefile, 'mean_cpc','ttime')
