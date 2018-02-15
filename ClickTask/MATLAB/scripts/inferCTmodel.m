ca8f73f64214220a2754a5055f71e70996318279
adrian <adrian.radillo@gmail.com>
2018-02-15 07:53:34 -0600

    reduce Euler dt

--- ClickTask/MATLAB/scripts/inferCTmodel.m
+++ ClickTask/MATLAB/scripts/inferCTmodel.m
@@ -16,7 +16,7 @@ clear
 rateLow=0.01;
 
 % time step for forward Euler, in sec
-dt=1/10000;
+dt=1/1000000;
 % max allowed change point count
 gamma_max=100;
 % hyperparameters for Gamma dist over hazard rate
@@ -35,7 +35,7 @@ cptimes=0.004;
 
 %% call ODE function
 %posttimes=1:50;
-posttimes=0.001:0.001:T;
+posttimes=0.001:0.00001:T;
 %posttimes=[0.1:0.1:.9, posttimes];
 posttimes(end)=T-2*dt;
 
@@ -47,10 +47,10 @@ title('post H+')
 ylabel('prob(H+)')
 xlabel('msec')
 xlim([0,11])
-ylim([0,1.05])
-line([SP SP],get(ax1,'YLim'),'Color',[1 0 0], 'LineWidth',2)
-line(get(ax1,'XLim'),[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
-line(get(ax1,'XLim'),[1,1],'Color',[0 0 0], 'LineWidth',1)
+%ylim([0,1.05])
+%line([SP SP],get(ax1,'YLim'),'Color',[1 0 0], 'LineWidth',2)
+%line(get(ax1,'XLim'),[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
+%line(get(ax1,'XLim'),[1,1],'Color',[0 0 0], 'LineWidth',1)
 ax1.FontSize=20;
 
 ax2=subplot(1,2,2);
@@ -59,10 +59,10 @@ title('post H-')
 ylabel('prob(H-)')
 xlabel('msec')
 xlim([0,11])
-ylim([0,1.05])
-line([SP SP],get(ax2,'YLim'),'Color',[1 0 0], 'LineWidth',2)
-line(get(ax2,'XLim'),[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
-line(get(ax2,'XLim'),[1,1],'Color',[0 0 0], 'LineWidth',1)
+%ylim([0,1.05])
+%line([SP SP],get(ax2,'YLim'),'Color',[1 0 0], 'LineWidth',2)
+%line(get(ax2,'XLim'),[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
+%line(get(ax2,'XLim'),[1,1],'Color',[0 0 0], 'LineWidth',1)
 ax2.FontSize=20;
 
 for snr=[4]
@@ -73,38 +73,42 @@ for snr=[4]
     ax1=subplot(2,2,1);
     ax1.FontSize=20;
     hold on
-    plot(1000*(posttimes), jointPost(1,:),'-bo','MarkerSize',10,'LineWidth',2)
-    line([SP SP],[0,1],'Color',[1 0 0], 'LineWidth',2)
-    line([0,10],[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
-    line([0,10],[1,1],'Color',[0 0 0], 'LineWidth',1)
+    plot(1000*(posttimes), unnorm(1,:),'-b','MarkerSize',10,'LineWidth',2)
+    %line([SP SP],[0,1],'Color',[1 0 0], 'LineWidth',2)
+    %line([0,10],[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
+    %line([0,10],[1,1],'Color',[0 0 0], 'LineWidth',1)
     xlim([0,10])
-    ylabel('posterior prob')
+    ylim([-1,7])
+    %ylabel('posterior prob')
     title('H+, a=0')
     
     ax2=subplot(2,2,2);
     ax2.FontSize=20;
     hold on
-    plot(1000*(posttimes), jointPost(gamma_max+1,:),'-bo','MarkerSize',10,...
+    plot(1000*(posttimes), unnorm(gamma_max+1,:),'-r','MarkerSize',10,...
         'LineWidth',2)
-    line([SP SP],[0,1],'Color',[1 0 0], 'LineWidth',2)
-    line([0,10],[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
-    line([0,10],[1,1],'Color',[0 0 0], 'LineWidth',1)
+   % line([SP SP],[0,1],'Color',[1 0 0], 'LineWidth',2)
+    %line([0,10],[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
+    %line([0,10],[1,1],'Color',[0 0 0], 'LineWidth',1)
     title('H-, a=0')
     xlim([0,10])
+    ylim([-1,7])
     
     ax3=subplot(2,2,3);
-    plot(1000*(posttimes), jointPost(2,:),'-o','LineWidth', ...
+    plot(1000*(posttimes), unnorm(2,:),'-','LineWidth', ...
         3, 'MarkerSize',8)
     title('H+, a=1')
     xlim([0,10])
-    ylabel('posterior prob')
+    ylim([-11,3])
+    %ylabel('posterior prob')
     xlabel('msec')
     ax3.FontSize=20;
     
     ax4=subplot(2,2,4);
-    plot(1000*(posttimes), jointPost(gamma_max+2,:),'-o','LineWidth', ...
+    plot(1000*(posttimes), unnorm(gamma_max+2,:),'-r','LineWidth', ...
         3, 'MarkerSize',8)
     xlim([0,10])
+    ylim([-11,3])
     xlabel('msec')
     title('H-, a=1')
     ax4.FontSize=20;
