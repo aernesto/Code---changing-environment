278ebf0971224b549c7c52e3b95ee0b981851374
adrian <adrian.radillo@gmail.com>
2018-02-16 19:27:39 -0600

    CT pass sim 2 goal with max CP=20

--- ClickTask/MATLAB/scripts/inferCTmodel.m
+++ ClickTask/MATLAB/scripts/inferCTmodel.m
@@ -18,7 +18,7 @@ rateLow=0.01;
 % time step for forward Euler, in sec
 dt=1/1000000;
 % max allowed change point count
-gamma_max=100;
+gamma_max=20;
 % hyperparameters for Gamma dist over hazard rate
 alpha=1;
 priorState=[.5,.5];
@@ -39,41 +39,83 @@ posttimes=0.0001:0.00001:T;
 posttimes(end)=T-2*dt;
 msect=1000*posttimes;
 
-fig1=figure(1);  
-%fig2=figure(2);
-SP=rTrain(1)*1000; %right click time in ms
-priorVar=1;
-snr=1;
-figure(fig1)
-%i=sub2ind([4,3],snr,priorVar);
-rates=[1,10,100];
-for ii=1:length(rates)
-    expRate=rates(ii);
-    ax=subplot(1,3,ii);
-    grid on
-    hold on
-    beta = alpha / sqrt(priorVar);
-    rateHigh=getlambdahigh(rateLow, snr, true);
-    % perform inference
-    [jointPost,~,priorGamma]=returnPostH(lTrain, rTrain, rateLow, rateHigh, T, ...
-        gamma_max, posttimes, priorState, alpha, beta, dt, cptimes,expRate);
-    
-    %compute marginal over CP count
-    marginalCPcount=jointPost(1:gamma_max,:)+...
-        jointPost(gamma_max+1:end,:); % dim = CPcount x TimeSteps
-    meanCPcount=(0:gamma_max-1)*marginalCPcount;
-    stdevCPcount=sqrt(((0:gamma_max-1).^2)*marginalCPcount-meanCPcount.^2);
-    % plot marginal
-    mylines=plot(msect, meanCPcount,'-b',...
-        msect,meanCPcount+stdevCPcount,'-k',...
-        msect,max(meanCPcount-stdevCPcount,0),'-k',...
-        'LineWidth', 3);
-    vertline=line([SP,SP],get(ax,'ylim'),'Color',[4,2,3]/4,'LineWidth',2);
-    title(['decay rate=',num2str(expRate)])
-    legend([mylines(1:2);vertline],...
-        'mean','1 stdev','click time','Location','NorthWest')
-    legend BOXOFF
-    ylabel('CP count')
-    xlabel('msec')
-    ax.FontSize=20;
+expRate=1; %exponential decay to apply to initial mass on a at t=0
+fig=figure(1);  
+SP=rTrain(1)*1000; %right click time in msec
+for priorVar=1:3
+    for snr=1:4
+        i=sub2ind([4,3],snr,priorVar);
+        ax=subplot(3,4,i);
+        grid on
+        hold on
+        beta = alpha / sqrt(priorVar);
+        rateHigh=getlambdahigh(rateLow, snr, true);
+        % perform inference
+        [jointPost,~]=returnPostH(lTrain, rTrain, rateLow, rateHigh, T, ...
+    gamma_max, posttimes, priorState, alpha, beta, dt, cptimes, expRate);
+        %compute marginal over CP count
+        marginalCPcount=jointPost(1:gamma_max,:)+...
+            jointPost(gamma_max+1:end,:); % dim = CPcount x TimeSteps
+        meanCPcount=(0:gamma_max-1)*marginalCPcount;
+        stdevCPcount=sqrt(((0:gamma_max-1).^2)*marginalCPcount-meanCPcount.^2);
+        % plot marginal
+        mylines=plot(msect, meanCPcount,'-b',...
+             msect,meanCPcount+stdevCPcount,'-k',...
+             msect,max(meanCPcount-stdevCPcount,0),'-k',...
+            'LineWidth', 3);
+        vertline=line([SP,SP],get(ax,'ylim'),'Color',[4,2,3]/4,'LineWidth',2);
+        title(['SNR=',num2str(snr),', Var=',num2str(priorVar)])
+        if snr==1 & priorVar==1
+            legend([mylines(1:2);vertline],...
+                'mean','1 stdev','click time','Location','NorthWest')
+            legend BOXOFF
+        end
+        if snr==1 
+            ylabel('CP count')
+        end
+        if priorVar==3
+            xlabel('msec')
+        end
+        %ylim([0,0.04])
+        ax.FontSize=20;
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% 
+% 
+% fig=figure(1); 
+% hax=axes;
+% SP=rTrain(1)*1000; %right click time in msec
+% hold on
+% title('posterior prob of H+ as fcn of time')
+% ylabel('posterior prob(H+)')
+% xlabel('msec')
+% xlim([0,11])
+% ylim([0.49,1.05])
+% line([SP SP],get(hax,'YLim'),'Color',[1 0 0], 'LineWidth',2)
+% hax.FontSize=20;
+% 
+% for snr=[.5,1,2,4]
+%     rateHigh=getlambdahigh(rateLow, snr, true);
+%     [jointPost,unnorm]=returnPostH(lTrain, rTrain, rateLow, rateHigh, T, ...
+%     gamma_max, posttimes, priorState, alpha, beta, dt, cptimes);
+%     plot(1000*posttimes, sum(jointPost(1:gamma_max,:),1),'LineWidth',2)
+% end
+% legend('click time','snr=0.5','snr=1','snr=2','snr=4')
+% line(get(hax,'XLim'),[0.5,.5],'Color',[0 0 0], 'LineWidth',1)
+% line(get(hax,'XLim'),[1,1],'Color',[0 0 0], 'LineWidth',1)
