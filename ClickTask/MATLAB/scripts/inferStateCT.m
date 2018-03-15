ddbe15e3597a03ae3f53fa04c5ac95f314e6213b
adrian <adrian.radillo@gmail.com>
2018-03-15 09:17:25 -0500

    add files I don't remember

---
+++ ClickTask/MATLAB/scripts/inferStateCT.m
@@ -0,0 +1,17 @@
+function postH=inferStateCT(lTrain, rTrain, rateLow, rateHigh, alpha,...
+    beta, dt_Euler, posttimes, priorStates, stimulusLength, gammax)
+% DESCRIPTION:
+%   returns the posterior over the state, given a stimulus and a set of
+%   parameters as input. This is the CT model (system of jump ODEs) with
+%   the maximum number of change points capped.
+%
+% ARGUMENTS:
+%   
+%
+%
+%
+%
+%
+%
+
+end
